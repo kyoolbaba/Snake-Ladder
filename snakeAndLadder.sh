8be393292e6e2131e1f24301b874e95e18b44fe3
kyoolbaba <kyoolbaba@gmail.com>
2020-04-15 15:12:15 +0530

    Refactored (allignment)

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -3,19 +3,19 @@ declare playerPosition=0
 declare DESTINATION=100
 
 play(){
-  while [[ $playerPosition -le $DESTINATION  ]]; do 
-  if [[ $playerPosition -lt '0' ]]; then
-      playerPosition=0
-  fi
-  playerPath[((count++))]=$playerPosition
-  dice=$((RANDOM%6 +1))
-  option=$((RANDOM%3 -1))
-  playerPosition=$(( playerPosition + (dice * option)))
-   if [[ $playerPosition -ge '101' ]]; then
-    ((count--))
-    playerPosition=$((playerPosition - dice))
-  fi 
-  done
+while [[ $playerPosition -le $DESTINATION  ]]; do 
+    if [[ $playerPosition -lt '0' ]]; then
+        playerPosition=0
+    fi
+playerPath[((count++))]=$playerPosition
+dice=$((RANDOM%6 +1))
+option=$((RANDOM%3 -1))
+playerPosition=$(( playerPosition + (dice * option)))
+    if [[ $playerPosition -ge '101' ]]; then
+        ((count--))
+        playerPosition=$((playerPosition - dice))
+    fi 
+done
 }
 
 play 
