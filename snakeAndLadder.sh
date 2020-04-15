b0925b330aebd3a266a8a6b0e49a4b4cfafff734
kyoolbaba <kyoolbaba@gmail.com>
2020-04-15 14:47:37 +0530

    Added feature of NoPlay/Ladder/Snake

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
-playerPosition[One]=0
-dice=$((RANDOM%6 + 1))
+playerPosition=0
+declare DESTINATION=100
+
+play(){
+  while [[ $playerPosition -le $DESTINATION  ]]; do 
+  if [[ $playerPosition -lt '0' ]]; then
+      playerPosition=0
+  fi
+  playerPath[((count++))]=$playerPosition
+  dice=$((RANDOM%6 +1))
+  option=$((RANDOM%3 -1))
+  playerPosition=$(( playerPosition + (dice * option)))
+  done
+}
+
+play 
