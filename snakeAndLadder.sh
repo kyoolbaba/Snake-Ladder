eacd472ffeaf0f7a6e693ee656f611086f8e3ee7
kyoolbaba <kyoolbaba@gmail.com>
2020-04-15 16:10:02 +0530

    Added second player

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,25 +2,37 @@
 declare playerPosition=0
 declare DESTINATION=100
 
-play(){
-while [[ $playerPosition -le $DESTINATION  ]]; do 
-    if [[ $playerPosition -lt '0' ]]; then
-        playerPosition=0
-    fi
-#POSITION OF EVERY POSITION AFTER A ROLL IS RECORDED IN ARRAY  playerPath below
-playerPath[((count++))]=$playerPosition 
+game(){
 dice=$((RANDOM%6 +1))
 option=$((RANDOM%3 -1))
 playerPosition=$(( playerPosition + (dice * option)))
-    if [[ $playerPosition -ge '101' ]]; then
-        ((count--))
-        playerPosition=$((playerPosition - dice))
-    fi 
-done
-#play count is stored here
-playcount=${#playerPath}
-
-
+    if [[ $playerPosition -lt '0' ]]; then
+        playerPosition=0
+    fi
 }
 
+play(){
+while [[ '1' -eq '1'  ]]; do 
+game  # CALLING METHOD TO CALCULATE THE DICE VALUE AND UPDATE THE POSITION
+playerPath1[((count++))]=$playerPosition #ALL POSITIONS OF PLAYER1 STORED HERE
+    if [[ $playerPosition -gt '100' ]]; then
+        report=Player1
+        break
+    fi
+game # CALLING METHOD TO CALCULATE THE DICE VALUE AND UPDATE THE POSITION
+playerPath2[((count++))]=$playerPosition #ALL POSITIONS OF PLAYER2 STORED HERE
+    if [[ $playerPosition -gt '100' ]]; then
+         report=Player2
+         break
+    fi
+done
+#play count is stored here
+playcount=${#playerPath1}
+}
 play 
+#UNCOMMENT THE BELOW LINES TO CHECK THE OUTPUTS
+#echo player 1
+#echo ${playerPath1[@]} 
+#echo player 2
+#echo ${playerPath2[@]}
+#echo $report won
