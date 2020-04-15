d60ee472e4db1995ea852332b42a7463e034a433
kyoolbaba <kyoolbaba@gmail.com>
2020-04-15 22:25:09 +0530

    Refactored (changed methods name and removed comments)

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,7 +2,7 @@
 declare playerPosition=0
 declare DESTINATION=100
 
-game(){
+rolldice(){
 dice=$((RANDOM%6 +1))
 option=$((RANDOM%3 -1))
 playerPosition=$(( playerPosition + (dice * option)))
@@ -11,28 +11,22 @@ playerPosition=$(( playerPosition + (dice * option)))
     fi
 }
 
-play(){
+playdicegame(){
 while [[ '1' -eq '1'  ]]; do 
-game  # CALLING METHOD TO CALCULATE THE DICE VALUE AND UPDATE THE POSITION
-playerPath1[((count++))]=$playerPosition #ALL POSITIONS OF PLAYER1 STORED HERE
+rolldice
+playerPath1[((count++))]=$playerPosition 
     if [[ $playerPosition -gt '100' ]]; then
-        report=Player1
+        report="Player1 has won" 
         break
     fi
-game # CALLING METHOD TO CALCULATE THE DICE VALUE AND UPDATE THE POSITION
-playerPath2[((count++))]=$playerPosition #ALL POSITIONS OF PLAYER2 STORED HERE
+rolldice 
+playerPath2[((count++))]=$playerPosition
     if [[ $playerPosition -gt '100' ]]; then
-         report=Player2
+         report="Player2 has won" 
          break
     fi
 done
 #play count is stored here
 playcount=${#playerPath1}
 }
-play 
-#UNCOMMENT THE BELOW LINES TO CHECK THE OUTPUTS
-#echo player 1
-#echo ${playerPath1[@]} 
-#echo player 2
-#echo ${playerPath2[@]}
-#echo $report won
+playdicegame 
