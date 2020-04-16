36f94cfbe160ad010dac9f7c7e8f504968bfb8fa
kyoolbaba <kyoolbaba@gmail.com>
2020-04-16 18:13:09 +0530

    Added feature for n players to play the game

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,48 +1,45 @@
 #! /bin/bash
 declare playerPosition=0
 declare DESTINATION=100
-declare playerPath1
-declare playerPath2
+declare -A playerPosition
+echo Enter the number of players
+read NO_OF_PLAYERS
 
 rolldice(){
-playerPosition=$1
-dice=$((RANDOM%6 +1))
-option=$((RANDOM%3 -1))
-playerPosition=$(( playerPosition + (dice * option)))
-    if [[ $playerPosition -lt '0' ]]; then
-        playerPosition=0
+    dice=$((RANDOM%6 +1))
+    option=$((RANDOM%3 -1))
+    playerPlace=$(( playerPlace + (dice * option)))
+    if [[ $playerPlace -lt '0' ]]; then
+        playerPlace=0
     fi
-    if [[ $playerPosition -gt '100' ]]; then
-        playerPosition=$(( playerPosition - dice))
+    if [[ $playerPlace -gt '100' ]]; then
+        playerPlace=$(( playerPlace - dice))
         echo hey
     fi
-   
 }
 
 playdicegame(){
     while [[ '1' -eq '1'  ]]; do 
-        prevPlayer1count=$((player1Count -1 ))
-        prevPlayer2count=$((player2Count -1 ))
-        rolldice ${playerPath1[$prevPlayer1count]}
-        playerPath1[((player1Count++))]=$playerPosition 
-        if [[ $playerPosition -ge '100' ]]; then
-            report="Player1 has won" 
-            break
-        fi
-        rolldice ${playerPath2[$prevPlayer2count]}
-        playerPath2[((player2Count++))]=$playerPosition
-        if [[ $playerPosition -ge '100' ]]; then
-            report="Player2 has won" 
+         ((count++))
+             for (( i = 1 ; i < $((NO_OF_PLAYERS+1)) ; i++  )); do 
+                 player=Player$i
+                 echo from $player  $player $player $player 
+                 playerPlace=${playerPosition[$player]}
+                 rolldice 
+                 playerPosition[Player$i]=$playerPlace 
+                 if [[ $playerPlace -ge '100' ]]; then
+                      report="Player$i has won" 
+                     break
+                 fi
+            done     
+        if [[ $playerPlace -ge '100' ]]; then
             break
         fi
     done
 #play count is stored here
-playcount=${#playerPath1[@]}
+playcount=$count
 }
-playdicegame 
 
-echo player 1
-echo ${playerPath1[@]} 
-echo player 2
-echo ${playerPath2[@]}
-echo $report won 
+playdicegame 
+echo $count
+echo $report
